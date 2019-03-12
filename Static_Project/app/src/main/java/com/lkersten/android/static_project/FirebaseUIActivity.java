29c8e460a7a65e5da20e47b01a008b31565b171b
Lane Kersten <>
2019-03-12 18:44:04 -0400

    Added project files to repo

--- Static_Project/app/src/main/java/com/lkersten/android/static_project/FirebaseUIActivity.java
+++
@@ -1,6 +0,0 @@
-package com.lkersten.android.static_project;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class FirebaseUIActivity extends AppCompatActivity {
-}
--- Static_Project/app/src/main/java/com/lkersten/android/static_project/MainActivity.java
+++ Static_Project/app/src/main/java/com/lkersten/android/static_project/MainActivity.java
@@ -3,13 +3,25 @@ package com.lkersten.android.static_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseApp.initializeApp(this);
 
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN);
     }
+
 }

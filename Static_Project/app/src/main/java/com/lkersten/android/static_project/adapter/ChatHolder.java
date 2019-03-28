f9644960d68a161f8a7de00fbfcfe9afa6ce31a0
Lane Kersten <>
2019-03-28 15:35:52 -0400

    Minor UI updates

--- Static_Project/app/src/main/java/com/lkersten/android/static_project/adapter/ChatHolder.java
+++ Static_Project/app/src/main/java/com/lkersten/android/static_project/adapter/ChatHolder.java
@@ -19,12 +19,14 @@ public class ChatHolder extends RecyclerView.ViewHolder {
     private final TextView mUsernameText;
     private final TextView mMessage;
     private final int mBlack;
+    private final int mAccent;
 
     public ChatHolder(@NonNull View itemView) {
         super(itemView);
         mUsernameText = itemView.findViewById(R.id.message_name);
         mMessage = itemView.findViewById(R.id.message_text);
         mBlack = ContextCompat.getColor(itemView.getContext(), R.color.black);
+        mAccent = ContextCompat.getColor(itemView.getContext(), R.color.colorAccent);
     }
 
     public void bind(Chat chat) {
@@ -53,10 +55,13 @@ public class ChatHolder extends RecyclerView.ViewHolder {
 
     private void setIsUser(boolean isUser) {
         if (isUser) {
+            mMessage.setTextColor(mAccent);
             mMessage.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
             mUsernameText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
         } else {
             mMessage.setTextColor(mBlack);
+            mMessage.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            mUsernameText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
         }
     }
 }
